425688157e64e51a97c6c225cf7d584ed94e14fa
Thagapillai12 <Thagu.meenachi@gmail.com>
2025-01-09 10:37:52 +0530

    Add files via upload

---
+++ BetterCoAPITests.Team.cs
@@ -0,0 +1,249 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WK.DE.AI.BetterCo.ApiClient;
+using WK.DE.AI.BetterCo.ViewModels;
+
+namespace WK.DE.BetterCo.IntegrationTests
+{
+    public partial class BetterCoAPITests
+    {
+        [TestMethod]
+        public void CreateAgentTeamMemberTest()
+        {
+            var betterCoApiClient = new BetterCoApiClient(betterCoConfiguration);
+
+            var workspaces = betterCoApiClient.GetWorkspaces();
+            var workspaceId = workspaces.Results.Where(x => string.Equals(x.DisplayName, betterCoConfiguration.WorkspaceName)).First().Id;
+
+            var createdTeammember = betterCoApiClient.CreateAgentTeamMember(workspaceId, "Max", "Mustermann", "max.mustermann@muster.de", "ext1");
+            Assert.IsNotNull(createdTeammember);
+            Assert.IsNotNull(createdTeammember.Id);
+
+            var deleteResponse = betterCoApiClient.DeleteTeammeber(workspaceId, createdTeammember.Id);
+            Assert.IsTrue(deleteResponse.StatusCode == System.Net.HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void CreateManagerTeamMemberTest()
+        {
+            var betterCoApiClient = new BetterCoApiClient(betterCoConfiguration);
+
+            var workspaces = betterCoApiClient.GetWorkspaces();
+            var workspaceId = workspaces.Results.Where(x => string.Equals(x.DisplayName, betterCoConfiguration.WorkspaceName)).First().Id;
+
+            var createdTeammember = betterCoApiClient.CreateManagerTeamMember(workspaceId, "Max", "Mustermann", "max.mustermann@muster.de", "ext1");
+            Assert.IsNotNull(createdTeammember);
+            Assert.IsNotNull(createdTeammember.Id);
+
+            var deleteResponse = betterCoApiClient.DeleteTeammeber(workspaceId, createdTeammember.Id);
+            Assert.IsTrue(deleteResponse.StatusCode == System.Net.HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void UpdateTeamMemberTest()
+        {
+            var betterCoApiClient = new BetterCoApiClient(betterCoConfiguration);
+
+            var workspaces = betterCoApiClient.GetWorkspaces();
+            var workspaceId = workspaces.Results.Where(x => string.Equals(x.DisplayName, betterCoConfiguration.WorkspaceName)).First().Id;
+
+            foreach (var item in betterCoApiClient.GetTeammembers(workspaceId).Results)
+            {
+                betterCoApiClient.DeleteTeammeber(workspaceId, item.Id);
+            }
+
+            var createdTeammember = betterCoApiClient.CreateManagerTeamMember(workspaceId, "Max", "Mustermann", "max.mustermann@muster.de", "ext1");
+            Assert.IsNotNull(createdTeammember);
+            Assert.IsNotNull(createdTeammember.Id);
+
+            var updatedTeammember = betterCoApiClient.UpdateTeamMember(workspaceId, createdTeammember.Id, "Maxi", "Mustermanni", "maxi.mustermanni@muster.de", "AGENT","ext1i");
+
+            Assert.IsTrue(updatedTeammember.Id.Equals(createdTeammember.Id, StringComparison.Ordinal));
+            Assert.IsTrue("Maxi".Equals(updatedTeammember.FirstName, StringComparison.Ordinal));
+            Assert.IsTrue("Mustermanni".Equals(updatedTeammember.LastName, StringComparison.Ordinal));
+            Assert.IsTrue("maxi.mustermanni@muster.de".Equals(updatedTeammember.Email, StringComparison.Ordinal));
+            Assert.IsTrue("ext1i".Equals(updatedTeammember.ExternalId, StringComparison.Ordinal));
+            Assert.IsNotNull(updatedTeammember.CreatedAt);
+            //Assert.IsTrue(updatedTeammember.CreatedAt < DateTime.Now);
+            //Assert.IsTrue(updatedTeammember.CreatedAt > DateTime.Now.AddSeconds(-2));
+            Assert.IsTrue("AGENT".Equals(updatedTeammember.Role, StringComparison.Ordinal));
+
+            var deleteResponse = betterCoApiClient.DeleteTeammeber(workspaceId, createdTeammember.Id);
+            Assert.IsTrue(deleteResponse.StatusCode == System.Net.HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void GetTeammembersTest()
+        {
+            var betterCoApiClient = new BetterCoApiClient(betterCoConfiguration);
+
+            var workspaces = betterCoApiClient.GetWorkspaces();
+            var workspaceId = workspaces.Results.Where(x => string.Equals(x.DisplayName, betterCoConfiguration.WorkspaceName)).First().Id;
+
+            var createdTeammember = betterCoApiClient.CreateManagerTeamMember(workspaceId, "Max", "Mustermann", "max.mustermann@muster.de", "ext1");
+            Assert.IsNotNull(createdTeammember);
+            Assert.IsNotNull(createdTeammember.Id);
+
+            var teammembers = betterCoApiClient.GetTeammembers(workspaceId);
+
+            Assert.IsNotNull(teammembers);
+            Assert.IsTrue(teammembers.Results.Count == 1);
+
+            var deleteResponse = betterCoApiClient.DeleteTeammeber(workspaceId, createdTeammember.Id);
+            Assert.IsTrue(deleteResponse.StatusCode == System.Net.HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void GetManagerTeammemberTest()
+        {
+            var betterCoApiClient = new BetterCoApiClient(betterCoConfiguration);
+
+            var workspaces = betterCoApiClient.GetWorkspaces();
+            var workspaceId = workspaces.Results.Where(x => string.Equals(x.DisplayName, betterCoConfiguration.WorkspaceName)).First().Id;
+
+            //foreach (var item in betterCoApiClient.GetTeammembers(workspaceId).Results)
+            //{
+            //    betterCoApiClient.DeleteTeammeber(workspaceId, item.Id);
+            //}
+
+            var createdTeammember = betterCoApiClient.CreateManagerTeamMember(workspaceId, "Max", "Mustermann", "max.mustermann@muster.de", "ext1");
+            Assert.IsNotNull(createdTeammember);
+            Assert.IsNotNull(createdTeammember.Id);
+
+            var teammember = betterCoApiClient.GetTeammember(workspaceId, createdTeammember.Id);
+
+            Assert.IsNotNull(teammember);
+            Assert.IsTrue(teammember.Id.Equals(createdTeammember.Id, StringComparison.Ordinal));
+            Assert.IsTrue("Max".Equals(teammember.FirstName, StringComparison.Ordinal));
+            Assert.IsTrue("Mustermann".Equals(teammember.LastName, StringComparison.Ordinal));
+            Assert.IsTrue("max.mustermann@muster.de".Equals(teammember.Email, StringComparison.Ordinal));
+            Assert.IsTrue("ext1".Equals(teammember.ExternalId, StringComparison.Ordinal));
+            Assert.IsNotNull(teammember.CreatedAt);
+            Assert.IsTrue(teammember.CreatedAt < DateTime.Now);
+            Assert.IsTrue(teammember.CreatedAt > DateTime.Now.AddSeconds(-2));
+            Assert.IsTrue("MANAGER".Equals(teammember.Role, StringComparison.Ordinal));
+
+            var deleteResponse = betterCoApiClient.DeleteTeammeber(workspaceId, createdTeammember.Id);
+            Assert.IsTrue(deleteResponse.StatusCode == System.Net.HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void GetAgentTeammemberTest()
+        {
+            var betterCoApiClient = new BetterCoApiClient(betterCoConfiguration);
+
+            var workspaces = betterCoApiClient.GetWorkspaces();
+            var workspaceId = workspaces.Results.Where(x => string.Equals(x.DisplayName, betterCoConfiguration.WorkspaceName)).First().Id;
+
+            //foreach (var item in betterCoApiClient.GetTeammembers(workspaceId).Results)
+            //{
+            //    betterCoApiClient.DeleteTeammeber(workspaceId, item.Id);
+            //}
+
+            var createdTeammember = betterCoApiClient.CreateAgentTeamMember(workspaceId, "Max", "Mustermann", "max.mustermann@muster.de", "ext1");
+            Assert.IsNotNull(createdTeammember);
+            Assert.IsNotNull(createdTeammember.Id);
+
+            var teammember = betterCoApiClient.GetTeammember(workspaceId, createdTeammember.Id);
+
+            Assert.IsNotNull(teammember);
+            Assert.IsTrue(teammember.Id.Equals(createdTeammember.Id, StringComparison.Ordinal));
+            Assert.IsTrue("Max".Equals(teammember.FirstName, StringComparison.Ordinal));
+            Assert.IsTrue("Mustermann".Equals(teammember.LastName, StringComparison.Ordinal));
+            Assert.IsTrue("max.mustermann@muster.de".Equals(teammember.Email, StringComparison.Ordinal));
+            Assert.IsTrue("ext1".Equals(teammember.ExternalId, StringComparison.Ordinal));
+            Assert.IsNotNull(teammember.CreatedAt);
+            Assert.IsTrue(teammember.CreatedAt < DateTime.Now);
+            Assert.IsTrue(teammember.CreatedAt > DateTime.Now.AddSeconds(-2));
+            Assert.IsTrue("AGENT".Equals(teammember.Role, StringComparison.Ordinal));
+
+            var deleteResponse = betterCoApiClient.DeleteTeammeber(workspaceId, createdTeammember.Id);
+            Assert.IsTrue(deleteResponse.StatusCode == System.Net.HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void GetTeammemberByExternalIdTest()
+        {
+            var betterCoApiClient = new BetterCoApiClient(betterCoConfiguration);
+
+            var workspaces = betterCoApiClient.GetWorkspaces();
+            var workspaceId = workspaces.Results.Where(x => string.Equals(x.DisplayName, betterCoConfiguration.WorkspaceName)).First().Id;
+
+            foreach (var item in betterCoApiClient.GetTeammembers(workspaceId).Results)
+            {
+                betterCoApiClient.DeleteTeammeber(workspaceId, item.Id);
+            }
+
+            var createdTeammember = betterCoApiClient.CreateAgentTeamMember(workspaceId, "Max", "Mustermann", "max.mustermann@muster.de", "ext1");
+            Assert.IsNotNull(createdTeammember);
+            Assert.IsNotNull(createdTeammember.Id);
+
+            var teammemberResults = betterCoApiClient.GetTeammemberByExternalId(workspaceId, "ext1");
+
+            Assert.IsNotNull(teammemberResults);
+            Assert.IsTrue(teammemberResults.Results.Count == 1);
+            Assert.IsTrue(teammemberResults.Results.First().Id.Equals(createdTeammember.Id, StringComparison.Ordinal));
+            Assert.IsTrue(teammemberResults.Results.First().DisplayName.Equals("Max Mustermann", StringComparison.Ordinal));
+
+            var deleteResponse = betterCoApiClient.DeleteTeammeber(workspaceId, createdTeammember.Id);
+            Assert.IsTrue(deleteResponse.StatusCode == System.Net.HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void GetTeammemberByEmailTest()
+        {
+            var betterCoApiClient = new BetterCoApiClient(betterCoConfiguration);
+
+            var workspaces = betterCoApiClient.GetWorkspaces();
+            var workspaceId = workspaces.Results.Where(x => string.Equals(x.DisplayName, betterCoConfiguration.WorkspaceName)).First().Id;
+
+            foreach (var item in betterCoApiClient.GetTeammembers(workspaceId).Results)
+            {
+                betterCoApiClient.DeleteTeammeber(workspaceId, item.Id);
+            }
+
+            var createdTeammember = betterCoApiClient.CreateAgentTeamMember(workspaceId, "Max", "Mustermann", "max.mustermann@muster.de", "ext1");
+            Assert.IsNotNull(createdTeammember);
+            Assert.IsNotNull(createdTeammember.Id);
+
+            var teammemberResults = betterCoApiClient.GetTeammemberByEmail(workspaceId, "ext1");
+
+            Assert.IsNotNull(teammemberResults);
+            Assert.IsTrue(teammemberResults.Results.Count == 1);
+            Assert.IsTrue(teammemberResults.Results.First().Id.Equals(createdTeammember.Id, StringComparison.Ordinal));
+            Assert.IsTrue(teammemberResults.Results.First().DisplayName.Equals("Max Mustermann", StringComparison.Ordinal));
+
+            var deleteResponse = betterCoApiClient.DeleteTeammeber(workspaceId, createdTeammember.Id);
+            Assert.IsTrue(deleteResponse.StatusCode == System.Net.HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void DeleteTeammemberTest()
+        {
+            var betterCoApiClient = new BetterCoApiClient(betterCoConfiguration);
+
+            var workspaces = betterCoApiClient.GetWorkspaces();
+            var workspaceId = workspaces.Results.Where(x => string.Equals(x.DisplayName, betterCoConfiguration.WorkspaceName)).First().Id;
+
+            //foreach (var item in betterCoApiClient.GetTeammembers(workspaceId).Results)
+            //{
+            //    betterCoApiClient.DeleteTeammeber(workspaceId, item.Id);
+            //}
+
+            var createdTeammember = betterCoApiClient.CreateAgentTeamMember(workspaceId, "Max", "Mustermann", "max.mustermann@muster.de", "ext1");
+            Assert.IsNotNull(createdTeammember);
+            Assert.IsNotNull(createdTeammember.Id);
+
+            var deleteResponse = betterCoApiClient.DeleteTeammeber(workspaceId, createdTeammember.Id);
+            Assert.IsTrue(deleteResponse.StatusCode == System.Net.HttpStatusCode.OK);
+
+            var teammember = betterCoApiClient.GetTeammember(workspaceId, createdTeammember.Id);
+
+            Assert.IsNull(teammember);
+        }
+    }
+}
---
+++ BetterCoApiClient.Team.cs
@@ -0,0 +1,133 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System.Collections.Generic;
+using WK.DE.AI.BetterCo.Json.Common;
+using WK.DE.AI.BetterCo.Json.Team;
+using WK.DE.AI.BetterCo.Mapping;
+
+namespace WK.DE.AI.BetterCo.ApiClient
+{
+    public partial class BetterCoApiClient
+	{
+        public BetterCoCreateResult CreateAgentTeamMember(string workspaceId, string firstName, string lastName, string email, string externalId)
+        {
+            return CreateTeamMember(workspaceId, firstName, lastName, email, StringConstants.BETTERCO_TEAM_AGENT, externalId);
+        }
+
+        public BetterCoCreateResult CreateManagerTeamMember(string workspaceId, string firstName, string lastName, string email, string externalId)
+        {
+            return CreateTeamMember(workspaceId, firstName, lastName, email, StringConstants.BETTERCO_TEAM_MANAGER, externalId);
+        }
+
+        private BetterCoCreateResult CreateTeamMember(string workspaceId, string firstName, string lastName, string email, string role, string externalId)
+		{
+			var authToken = GetAuthToken();
+
+			string methodUrl = "/workspaces/" + workspaceId + "/team";
+
+			var response = DoPostRequest(methodUrl, authToken, new BetterCoCreateTeammemberBody() { FirstName = firstName, LastName = lastName, Email = email, Role = role, ExternalId = externalId });
+
+			BetterCoCreateResult createResult = null;
+			if (response != null)
+			{
+				createResult = JsonConvert.DeserializeObject<BetterCoCreateResult>(response.Content);
+			}
+
+			return createResult;
+		}
+
+        public BetterCoTeammember UpdateTeamMember(string workspaceId, string teammemberId, string firstName, string lastName, string email, string role, string externalId)
+        {
+            var authToken = GetAuthToken();
+
+            string methodUrl = "/workspaces/" + workspaceId + "/team/" + teammemberId;
+
+            var response = DoPatchRequest(methodUrl, authToken, new BetterCoCreateTeammemberBody() { FirstName = firstName, LastName = lastName, Email = email, Role = role, ExternalId = externalId });
+
+            BetterCoTeammember updateTeamMember = null;
+            if (response != null)
+            {
+                updateTeamMember = JsonConvert.DeserializeObject<BetterCoTeammember>(response.Content);
+            }
+
+            return updateTeamMember;
+        }
+
+        public BetterCoResults GetTeammembers(string workspaceId)
+        {
+            var authToken = GetAuthToken();
+
+            string methodUrl = "/workspaces/" + workspaceId + "/team";
+
+            var response = DoGetRequest(methodUrl, authToken);
+
+            BetterCoResults betterCoResults = null;
+            if (response != null)
+            {
+                betterCoResults = JsonConvert.DeserializeObject<BetterCoResults>(response.Content);
+            }
+
+            return betterCoResults;
+        }
+
+        public BetterCoTeammember GetTeammember(string workspaceId, string teammemberId)
+        {
+            var authToken = GetAuthToken();
+
+            string methodUrl = "/workspaces/" + workspaceId + "/team/" + teammemberId;
+
+            var response = DoGetRequest(methodUrl, authToken);
+
+            BetterCoTeammember teammember = null;
+            if (response != null)
+            {
+                teammember = JsonConvert.DeserializeObject<BetterCoTeammember>(response.Content);
+            }
+
+            return teammember;
+        }
+
+        public BetterCoResults GetTeammemberByExternalId(string workspaceId, string externalId)
+        {
+            var authToken = GetAuthToken();
+
+            string methodUrl = "/workspaces/" + workspaceId + "/team";
+
+            var response = DoGetRequest(methodUrl, authToken, new List<KeyValuePair<string, string>>() { new KeyValuePair<string, string>("query", externalId) });
+
+            BetterCoResults betterCoResults = null;
+            if (response != null)
+            {
+                betterCoResults = JsonConvert.DeserializeObject<BetterCoResults>(response.Content);
+            }
+
+            return betterCoResults;
+        }
+
+        public BetterCoResults GetTeammemberByEmail(string workspaceId, string email)
+        {
+            var authToken = GetAuthToken();
+
+            string methodUrl = "/workspaces/" + workspaceId + "/team";
+
+            var response = DoGetRequest(methodUrl, authToken, new List<KeyValuePair<string, string>>() { new KeyValuePair<string, string>("query", email) });
+
+            BetterCoResults betterCoResults = null;
+            if (response != null)
+            {
+                betterCoResults = JsonConvert.DeserializeObject<BetterCoResults>(response.Content);
+            }
+
+            return betterCoResults;
+        }
+
+        public IRestResponse DeleteTeammeber(string workspaceId, string teammemberId)
+        {
+            var authToken = GetAuthToken();
+
+            string methodUrl = "/workspaces/" + workspaceId + "/team/" + teammemberId;
+
+            return DoDeleteRequest(methodUrl, authToken);
+        }
+    }
+}
